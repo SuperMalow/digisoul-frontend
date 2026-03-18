b907861194b743b8a7ce3d947cf7cb3803dd9ade
shengwenmacmini <shengwen9690@163.com>
2026-03-19 04:42:44 +0800

    fix: fix base url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.resolve();
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
